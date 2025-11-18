febaacbc10f4cfbf5c0a5b9dab557c96cad7cd23
dominikkoenitzer <dominik.koenitzer@gmail.com>
2025-11-18 13:41:28 +0100

    Add esbuild configuration to optimize build performance

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,10 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  esbuild: {
+    target: 'es2020',
+    legalComments: 'none',
+  },
   build: {
     rollupOptions: {
       output: {
